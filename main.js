96d015cd7d3520ec79a13798a397733947f16ee0
Ahmad Afham <54086061+aangamers021@users.noreply.github.com>
2023-12-02 03:14:50 +0800

    Update main.js

--- main.js
+++ main.js
@@ -4,8 +4,22 @@ document.addEventListener('DOMContentLoaded', function () {
 
         try {
             const response = await fetch(`https://jup-airdrop.zhen8558.workers.dev/allocation/${walletAddress}`);
-            const data = await response.text();
-            document.getElementById('result').innerHTML = data;
+            const data = await response.json(); // Mengubah respons menjadi objek JSON
+
+            // Mengambil nilai yang diinginkan dari properti JSON
+            const owner = data.owner;
+            const tokensFinal = data.tokens_final;
+            const volume = data.volume;
+            const rank = data.debug_rank;
+
+            // Menampilkan hasil di dalam elemen dengan id 'result'
+            const resultElement = document.getElementById('result');
+            resultElement.innerHTML = `
+                <p>Owner: ${owner}</p>
+                <p>Tokens Final: ${tokensFinal}</p>
+                <p>Volume: ${volume}</p>
+                <p>Rank: ${rank}</p>
+            `;
         } catch (error) {
             console.error("Error fetching data:", error);
             document.getElementById('result').innerHTML = "Error fetching data.";
