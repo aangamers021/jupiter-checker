1f41e217072a2e58f0d1547227329a88f74032a5
Ahmad Afham <54086061+aangamers021@users.noreply.github.com>
2023-12-02 03:32:53 +0800

    Update main.js

--- main.js
+++ main.js
@@ -9,16 +9,20 @@ document.addEventListener('DOMContentLoaded', function () {
             // Mengambil nilai yang diinginkan dari properti JSON
             const owner = data.owner;
             const tokensFinal = data.tokens_final;
+            const og = data.tokens_og_bonus;
             const volume = data.volume;
             const rank = data.debug_rank;
+            const swapScore = data.debug_swap_score;
 
             // Menampilkan hasil di dalam elemen dengan id 'result'
             const resultElement = document.getElementById('result');
             resultElement.innerHTML = `
                 <p>Wallet Address: ${owner}</p>
                 <p>Tokens Final: ${tokensFinal}</p>
+                <p>Tokens OG Bonus: ${og}</p>
                 <p>Volume TX: ${volume}</p>
                 <p>Rank: ${rank}</p>
+                <p>Swap Score: ${swapScore}</p>
             `;
         } catch (error) {
             console.error("Error fetching data:", error);
